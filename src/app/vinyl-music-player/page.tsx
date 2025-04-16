dd36fb4098839cc3fb072c1b36c54c2a7197b449
Neel Raval <neel.raval@odysseytherapeia.com>
2025-04-16 18:24:07 +0530

    making a small change

--- src/app/vinyl-music-player/page.tsx
+++ src/app/vinyl-music-player/page.tsx
@@ -1,7 +1,7 @@
 const VinylMusicPlayerPage: React.FC = () => {
     return (
       <main className="h-screen w-full flex items-center justify-center">
-        <h1 className="text-4xl font-bold">🎶 Vinyl Music Player</h1>
+        <h1 className="text-4xl font-bold">🎶 Neelluu's Vinyl Music Player</h1>
       </main>
     );
   };
